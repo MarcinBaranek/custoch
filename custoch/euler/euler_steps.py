01501b2413d1b1a4e4304023d893ae4035f24339
MarcinBaranek <baranekmarcin47@gmail.com>
2023-06-04 16:18:42 +0200

    Add Weak approx

--- custoch/euler/euler_steps.py
+++ custoch/euler/euler_steps.py
@@ -170,15 +170,30 @@ class EulerPath(EulerStep):
     N: int = 100
 
     def get_kernel(
-            self, with_user_dw: bool = False
+            self,
+            with_user_dw: bool = False,
+            only_last: bool = False,
+            with_off_set: bool = False
     ) -> Callable[
             [Time, Vector[d, One], float, Out[d, One], RandomState], None
     ] | Callable:
         euler_step = super(self.__class__, self).get_kernel(with_user_dw=False)
         dt = (self.T - self.t_0) / self.N
-        return self.get_euler_path(
-            euler_step, self.dim, self.precision.value, self.N, self.t_0, dt
-        )
+        if only_last:
+            return self.get_euler_path_only_last(
+                euler_step, self.dim, self.precision.value, self.N, self.t_0,
+                dt
+            )
+        elif with_off_set:
+            return self.get_euler_path_with_off_set(
+                euler_step, self.dim, self.precision.value, self.N, self.t_0,
+                dt
+            )
+        else:
+            return self.get_euler_path(
+                euler_step, self.dim, self.precision.value, self.N, self.t_0,
+                dt
+            )
 
     @staticmethod
     def get_euler_path(
@@ -205,3 +220,54 @@ class EulerPath(EulerStep):
                     out[j, i] = tmp_out[j, 0]
                 write_from_to(tmp_out, tmp_in)
         return __euler_path
+
+    @staticmethod
+    def get_euler_path_with_off_set(
+            euler_step: Callable[
+                [Time, Vector[d, One], float, Out[d, One], RandomState],
+                None
+            ], dim, precision, n_steps: int, start_time: float, dt: float
+    ):
+        @cuda.jit(device=True)
+        def __euler_path_with_off_set(
+                point: cuda.device_array, off_set, out: cuda.device_array,
+                state: cuda.device_array
+        ):
+            tmp_in = cuda.local.array(shape=(dim, 1), dtype=precision)
+            tmp_out = cuda.local.array(shape=(dim, 1), dtype=precision)
+            write_from_to(point, tmp_in)
+            cur_time = start_time
+            for i in range(n_steps):
+                euler_step(cur_time, tmp_in, dt, tmp_out, state)
+
+                cur_time = cur_time + dt
+                # COLLECTOR
+                for j in range(dim):
+                    out[j + off_set, i] = tmp_out[j, 0]
+                write_from_to(tmp_out, tmp_in)
+
+        return __euler_path_with_off_set
+
+    @staticmethod
+    def get_euler_path_only_last(
+            euler_step: Callable[
+                [Time, Vector[d, One], float, Out[d, One], RandomState],
+                None
+            ], dim, precision, n_steps: int, start_time: float, dt: float
+    ):
+        @cuda.jit(device=True)
+        def __euler_path_only_last(
+                point: cuda.device_array, out: cuda.device_array,
+                state: cuda.device_array
+        ):
+            tmp_in = cuda.local.array(shape=(dim, 1), dtype=precision)
+            tmp_out = cuda.local.array(shape=(dim, 1), dtype=precision)
+            write_from_to(point, tmp_in)
+            cur_time = start_time
+            for i in range(n_steps):
+                euler_step(cur_time, tmp_in, dt, tmp_out, state)
+                cur_time = cur_time + dt
+            # COLLECTOR
+            write_from_to(tmp_out, out)
+
+        return __euler_path_only_last
