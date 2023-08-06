7069c897ae45bb3e482c81827ee19c4e7098a4c0
MarcinBaranek <baranekmarcin47@gmail.com>
2023-08-06 13:04:46 +0200

    Fix problem with precisions

--- tests/test_precision.py
+++ tests/test_precision.py
@@ -1,6 +1,6 @@
 import pytest
 import numpy as np
-from custoch.precision import BasePrecision
+from custoch.precision import BasePrecision, Precisions
 
 
 @pytest.mark.parametrize('precision', ['float16', 'float32', 'float64'])
@@ -13,3 +13,15 @@ def test_set_ok_precision(precision: str):
 def test_set_nok_precision(precision: str):
     with pytest.raises(ValueError):
         BasePrecision(precision=precision)
+
+
+@pytest.mark.parametrize('precision', [Precisions.float16, Precisions.float64])
+def test_set_custoch_precision(precision: str):
+    obj = BasePrecision(precision=precision)
+    assert obj.precision == getattr(np, precision)
+
+
+@pytest.mark.parametrize('precision', [np.float32, np.float64])
+def test_set_numpy_precision(precision: str):
+    obj = BasePrecision(precision=precision)
+    assert obj.precision == precision
